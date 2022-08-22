9ac160683fe54c5e9873856b72cbaefed602e521
Erol Dalkılıç <eroldalg@gmail.com>
2022-08-22 12:42:38 +0300

    save

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -11,18 +11,40 @@ namespace ToDoList.Controllers
     {
         public ActionResult Listele()
         {
+            //arama motoru
+
             TodolistEntitiesConnectionstring db = new TodolistEntitiesConnectionstring();
             ViewBag.liste = db.lists;
             return View();
         }
         [HttpPost]
-        public ActionResult Kaydet(list a)
+        public ActionResult Kaydet(list a,String Title,string Description)
         {
             TodolistEntitiesConnectionstring db = new TodolistEntitiesConnectionstring();
             db.lists.Add(a);
             db.SaveChanges();
             return RedirectToAction("Listele");
         }
+        [HttpPost]
+        public ActionResult Duzenle(list d)
+        {
+            TodolistEntitiesConnectionstring db = new TodolistEntitiesConnectionstring();
+            list KL = db.lists.FirstOrDefault(x => x.id == d.id);
+            KL.Title = d.Title;
+            KL.Description = d.Description;
+            db.SaveChanges();
+            return RedirectToAction("Listele");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            TodolistEntitiesConnectionstring db = new TodolistEntitiesConnectionstring();
+            list silinecek = db.lists.FirstOrDefault(x => x.id == id);
+            db.lists.Remove(silinecek);
+            db.SaveChanges();
+            return RedirectToAction("Listele");
+        }
+
 
 
     }
